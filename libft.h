5b7485e6073695e9c3b2649190031ffdb1204c35
Smashkat12 <smashkat12@gmail.com>
2019-05-24 23:18:01 +0200

    new fuctions added: ft_strcpy and ft_strlen'

--- libft.h
+++ libft.h
@@ -59,7 +59,7 @@ void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 
-int				ft_strlen(const char *str);
+size_t				ft_strlen(const char *s);
 char				*ft_strdup(const char *s1);
 char				*ft_strndup(const char *s1, size_t n);
 char				*ft_strcpy(char *dst, const char *src);
@@ -82,8 +82,6 @@ int					ft_isascii(int c);
 int					ft_isprint(int c);
 int					ft_toupper(int c);
 int					ft_tolower(int c);
-int					ft_isupper(int c);
-int					ft_islower(int c);
 
 # ifndef IS_SPACE
 #  define IS_SPACE(x) (x==' '||x=='\n'||x=='\t')
