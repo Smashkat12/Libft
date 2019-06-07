f525fc5cdfd68dc25a3e9f7126ce62fea57e4527
Smashkat12 <smashkat12@gmail.com>
2019-06-07 16:28:19 +0200

    adding functions that were on laptop

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -14,7 +14,11 @@
 
 void	ft_putnbr(int n)
 {
-	if (n < 0)
+	if (n == -2147483648)
+	{
+		ft_putstr("-2147483648");
+	}
+	else if (n < 0)
 	{
 		ft_putchar('-');
 		ft_putnbr(-n);
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,7 +14,11 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648, fd");
+	}
+	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		ft_putnbr_fd(-n, fd);
