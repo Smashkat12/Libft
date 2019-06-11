55a059ba893291fc6b9f3e8fb9732b4ecc9ed523
Katlego MORULANE <kmorulan@c4r11s3.wethinkcode.co.za>
2019-06-11 13:29:55 +0200

    last functions include 42 header

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -5,72 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmorulan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/10 16:18:16 by kmorulan          #+#    #+#             */
-/*   Updated: 2019/06/10 17:04:15 by kmorulan         ###   ########.fr       */
+/*   Created: 2019/06/11 13:24:35 by kmorulan          #+#    #+#             */
+/*   Updated: 2019/06/11 13:24:47 by kmorulan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static  int        count_wrds(char const *str, char c)
+static	int		count_wrds(char const *str, char c)
 {
-    int word;
-    int i;
-    word = 0;
-    i = 0;
-    while (str[i])
-    {
-        while ((str[i] == c) && (str[i] != '\0'))
-        {
-            i++;
-        }
-        if ((str[i] != c) && (str[i] != '\0'))
-        {
-            while ((str[i] != c) && (str[i] != '\0'))
-            {
-                i++;
-            }
-            word++;
-        }
-    }
-    return (word);
-}
-char    **ft_strsplit(char const *s, char c)
-{
-    char    **word_str;
-    int        r;
-    int        i;
-    int        j;
-    int        words;
+	int word;
+	int i;
 
-    r = 0;
-    i = 0;
-    j = 0;
-    words = count_wrds(s, c);
-    word_str =(char **)malloc(words * sizeof(char *));
-	if (word_str)
+	word = 0;
+	i = 0;
+	while (str[i])
 	{
-    	while (r < words)
-    	{
-        	while ((s[i] == c) && (s[i] != '\0'))
-            	i++;
-        	if ((s[i] != c) && (s[i] != '\0'))
-        	{
-            	j = 0;
-            	while ((s[i + j] != c) && (s[i + j] != '\0'))
-                	j++;
-            	word_str[r] = (char *)malloc(j + 1);
-            	j = 0;
-            	while ((s[i] != c) && (s[i] != '\0'))
-                	word_str[r][j++] = s[i++];
-            	word_str[r][j] = '\0';
-        	}
-        r++;
-    	}
-    	return (word_str);
+		while ((str[i] == c) && (str[i] != '\0'))
+		{
+			i++;
+		}
+		if ((str[i] != c) && (str[i] != '\0'))
+		{
+			while ((str[i] != c) && (str[i] != '\0'))
+			{
+				i++;
+			}
+			word++;
+		}
 	}
-	else
-		return (NULL);
+	return (word);
 }
 
+char			**ft_strsplit(char const *s, char c)
+{
+	char	**word_str;
+	int		r;
+	int		i;
+	int		j;
 
+	r = 0;
+	i = 0;
+	j = 0;
+	if ((word_str = (char **)malloc(count_wrds(s, c) * sizeof(char *))))
+	{
+		while (r++ < count_wrds(s, c))
+			while ((s[i++] == c) && (s[i] != '\0'))
+				if ((s[i] != c) && (s[i] != '\0'))
+				{
+					j = 0;
+					while ((s[i + j++] != c) && (s[i + j] != '\0'))
+						word_str[r] = (char *)malloc(j + 1);
+					j = 0;
+					while ((s[i] != c) && (s[i] != '\0'))
+						word_str[r][j++] = s[i++];
+					word_str[r][j] = '\0';
+				}
+		return (word_str);
+	}
+	return (NULL);
+}
---
+++ ft_strtrim.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmorulan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 13:25:45 by kmorulan          #+#    #+#             */
+/*   Updated: 2019/06/11 13:25:56 by kmorulan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	void	ft_start_check(char const *s, size_t *i)
+{
+	while ((s[*i] == ' ') || (s[*i] == '\n') || (s[*i] == '\t'))
+	{
+		(*i)++;
+	}
+}
+
+static	void	ft_end_check(char const *s, size_t *j)
+{
+	*j = ft_strlen(s) - 1;
+	while ((s[*j] == ' ') || (s[*j] == '\n') || (s[*j] == '\t'))
+	{
+		(*j)--;
+	}
+}
+
+char			*ft_strtrim(char const *s)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	*str;
+
+	i = 0;
+	k = 0;
+	ft_start_check(s, &i);
+	if (s[i] == '\0')
+	{
+		str = (char *)malloc(sizeof(char) * 1);
+		str[i] = s[i];
+		return (str);
+	}
+	ft_end_check(s, &j);
+	str = (char *)malloc(sizeof(char) * (j - i) + 2);
+	if (str == NULL)
+		return (NULL);
+	while ((i + k) <= j)
+	{
+		str[k] = s[i + k];
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
